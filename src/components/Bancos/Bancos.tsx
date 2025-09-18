33ac78d99f68e48fc195fb844cce6bcc6b05adb5
Ivan <ivanlopezokk@gmail.com>
2025-09-17 22:34:53 -0300

    componente

---
+++ src/components/Bancos/Bancos.tsx
@@ -0,0 +1,21 @@
+
+
+
+const Banco = () =>{
+
+    return(
+    <section> 
+        <div className="container">
+            <h2>El auto que queres con la financiacion que necesitas!</h2>
+            <div className="container-x">
+                <div>
+                    <img />
+                </div>
+            </div>
+        </div>
+
+    </section>
+    )
+}
+
+export default Banco;
